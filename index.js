a2d5fe56b0a2b4419fcb6e2594ea7ca5eb5766d5
Justin <justin.kiarie@student.moringaschool.com>
2022-08-18 14:05:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blocks =  someValue - 42;
+    if (someValue > 42){
+        return blocks
+    }else (someValue < 42);{
+        return 42 - someValue
+    }//returns the number of blocks given a value
+  }// Code your solution in this file!
+
+
+function distanceFromHqInFeet(someValue) {
+  let feet = distanceFromHqInBlocks(someValue);
+  if  (true){
+    return feet * 264
+  }
+
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination){
+        let sum = start - destination
+        return sum * 264
+    } else if (start < destination) {
+        let sum = destination - start
+        return sum * 264
+    }//returns the number of feet traveled
+  }
+
+
+  function calculatesFarePrice(start, destination) {
+    let fee = distanceTravelledInFeet(start, destination)
+    let fare = fee - 400
+    if(fee < 400){
+        return 0
+    }else if (fare < 2000){
+        return fare * 2/100
+    }else if (2000 < fee > 2500){
+        return 25
+    }else if (fee > 2500){
+        return 'cannot travel that far'
+    }//returns the fare for the customer
+  }
